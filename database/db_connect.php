17fa67795ce2004e56d8d74e65b8781d1c7575e1
Mohammad Syahmi <116091141+msyaa8848@users.noreply.github.com>
2025-08-09 11:56:47 +0800

    add db file via upload
    to systematic for db connection

---
+++ database/db_connect.php
@@ -0,0 +1,29 @@
+<?php
+
+$host = "localhost";
+
+$username = "root";
+
+$password = "";
+
+$database = "webprojectgroup";
+
+
+
+// Create connection
+
+$conn = new mysqli($host, $username, $password, $database);
+
+
+
+// Check connection
+
+if ($conn->connect_error) {
+
+    die("Connection failed: " . $conn->connect_error);
+
+}
+
+
+
+?>
